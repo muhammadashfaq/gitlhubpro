4c66760f6081064aa9b4d6d806eb3b7add8d346e
adeel111 <adeeliftikhar67@gmail.com>
2019-12-02 10:48:12 -0800

    completed requested changes

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 console.log("Welcome to Github");
- 
+
 console.log("My very first commit for new feature");
- 
+
 console.log("created new branch (dashboard)");
+
+console.log("completed requested changes");
